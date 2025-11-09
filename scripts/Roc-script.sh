f88b90519aadf71b81618bb2f703cfcab0af4f20
deglint <89761518+deglint@users.noreply.github.com>
2025-11-09 15:45:51 +0800

    Update Roc-script.sh

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -87,5 +87,8 @@ git clone --depth=1 https://github.com/lwb1978/openwrt-gecoosac package/openwrt-
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
 
+# 添加 Aurora 主题
+git clone --depth=1 https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
